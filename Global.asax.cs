bc9467e4c5c6a16899b03949aebf14d4c1a0da77
Jess Buman <jessica-buman@uiowa.edu>
2020-07-26 17:54:18 -0500

    j27

--- Global.asax.cs
+++ Global.asax.cs
@@ -18,17 +18,6 @@ namespace MentorAppFinal
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        void Application_Error(object sender, EventArgs e)
-        {
-            Exception exc = Server.GetLastError();
-
-            if (exc is HttpUnhandledException)
-            {
-                // Pass the error on to the error page.
-                Server.Transfer("Error", true);
-            }
-        }
-
     }
     
     
